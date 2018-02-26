5d9abb0df7bf17b015e8ae70599636a97d84f7f3
Ivan <ivandodo@gmail.com>
2018-02-26 11:29:34 +0100

    Section 3 Lession 12

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,7 +4,8 @@ console.log('App.js is running');
 
 var app = {
     title: 'Indecision app',
-    subtitle: 'Subtitle'
+    subtitle: 'Put your life in the hands of a computer',
+    options: ['One', 'Two']
 };
 
 var template = React.createElement(
@@ -15,10 +16,29 @@ var template = React.createElement(
         null,
         app.title
     ),
-    React.createElement(
+    app.subtitle && React.createElement(
         'p',
         null,
         app.subtitle
+    ),
+    React.createElement(
+        'p',
+        null,
+        app.options.length > 0 ? 'Here are your options' : 'No options'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
     )
 );
 
@@ -34,26 +54,30 @@ var user = {
     location: 'Belgrade'
 };
 
+function getLocation(location) {
+    return location ? React.createElement(
+        'p',
+        null,
+        'Location: ',
+        location
+    ) : undefined;
+}
+
 var template2 = React.createElement(
     'div',
     null,
     React.createElement(
         'h1',
         null,
-        user.name
+        user.name ? user.name : 'Anonimous'
     ),
-    React.createElement(
+    user.age && user.age >= 18 && React.createElement(
         'p',
         null,
         'Age : ',
         user.age
     ),
-    React.createElement(
-        'p',
-        null,
-        'Location: ',
-        user.location
-    )
+    getLocation(user.location)
 );
 
 ReactDOM.render(template, appRoot);
