7b5620eae63a27009a937bfdeeb729d32274cf55
Ivan <ivandodo@gmail.com>
2018-02-26 14:27:04 +0100

    Lecture 20

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,87 +5,86 @@ console.log('App.js is running');
 var app = {
     title: 'Indecision app',
     subtitle: 'Put your life in the hands of a computer',
-    options: ['One', 'Two']
+    selections: []
 };
 
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        app.title
-    ),
-    app.subtitle && React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.options.length > 0 ? 'Here are your options' : 'No options'
-    ),
-    React.createElement(
-        'ol',
-        null,
-        React.createElement(
-            'li',
-            null,
-            'Item one'
-        ),
-        React.createElement(
-            'li',
-            null,
-            'Item two'
-        )
-    )
-);
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
 
-var appRoot = document.getElementById('app');
-
-var count = 0;
-var addOne = function addOne() {
-    count++;
-    renderCounterApp();
-};
-var minusOne = function minusOne() {
-    count--;
-    renderCounterApp();
-};
-var reset = function reset() {
-    count = 0;
-    renderCounterApp();
+    var option = e.target.elements.option.value;
+    if (option) {
+        app.selections.push(option);
+        e.target.elements.option.value = '';
+        renderApp();
+    }
 };
 
-var renderCounterApp = function renderCounterApp() {
-    var template2 = React.createElement(
+var removeAll = function removeAll() {
+    if (app.selections.length > 0) {
+        app.selections = [];
+        renderApp();
+    }
+};
+
+var makeDecision = function makeDecision() {
+    var randomNum = Math.floor(Math.random() * app.selections.length);
+    alert(app.selections[randomNum]);
+};
+
+var renderApp = function renderApp() {
+    var template = React.createElement(
         'div',
         null,
         React.createElement(
             'h1',
             null,
-            'Count: ',
-            count
+            app.title
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.selections.length > 0 ? 'Here are your options' : 'No options'
         ),
         React.createElement(
             'button',
-            { onClick: addOne },
-            '+1'
+            { disabled: app.selections.length === 0, onClick: makeDecision },
+            'What should I do?'
         ),
         React.createElement(
             'button',
-            { onClick: minusOne },
-            '-1'
+            { onClick: removeAll },
+            'Remove all'
         ),
         React.createElement(
-            'button',
-            { onClick: reset },
-            'reset'
+            'ol',
+            null,
+            app.selections.map(function (selection) {
+                return React.createElement(
+                    'li',
+                    { key: selection },
+                    selection
+                );
+            })
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                { type: 'submit' },
+                'Add option'
+            )
         )
     );
-
-    ReactDOM.render(template2, appRoot);
+    ReactDOM.render(template, appRoot);
 };
 
-renderCounterApp();
+var appRoot = document.getElementById('app');
+
+renderApp();
--- src/app.js
+++ src/app.js
@@ -3,50 +3,55 @@ console.log('App.js is running');
 let app = {
     title:'Indecision app',
     subtitle:'Put your life in the hands of a computer',
-    options:['One','Two']
+    selections:[]
 };
 
-let template = (
-    <div>
-        <h1>{app.title}</h1>
-        {app.subtitle && <p>{app.subtitle}</p>}
-        <p>{app.options.length > 0 ? 'Here are your options' : 'No options'}</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
-        </ol>
-    </div>
-);
+const onFormSubmit = (e) => {
+    e.preventDefault();
+    
+    const option = e.target.elements.option.value;
+    if (option){
+        app.selections.push(option);
+        e.target.elements.option.value = '';
+        renderApp();
+    }
+};
+
+const removeAll = () => {
+    if (app.selections.length > 0){
+        app.selections = [];
+        renderApp();
+    }
+};
+
+const makeDecision = () => {
+    const randomNum = Math.floor(Math.random() * app.selections.length);
+    alert(app.selections[randomNum]);
+}
+
+const renderApp = () => {
+    let template = (
+        <div>
+            <h1>{app.title}</h1>
+            {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{app.selections.length > 0 ? 'Here are your options' : 'No options'}</p>
+            <button disabled={app.selections.length===0} onClick={makeDecision}>What should I do?</button>
+            <button onClick={removeAll}>Remove all</button>
+            <ol>
+                {
+                    app.selections.map( (selection) =><li key={selection}>{selection}</li>)
+                }
+            </ol>
+            <form onSubmit={onFormSubmit}>
+                <input type="text" name="option" />
+                <button type="submit">Add option</button>
+            </form>
+        </div>
+    );
+    ReactDOM.render(template,appRoot);    
+}
 
 const appRoot = document.getElementById('app');
 
-let count = 0;
-const addOne = () => {
-    count++;
-    renderCounterApp();
-};
-const minusOne = () => {    
-    count--;
-    renderCounterApp();
-};
-const reset = () => {    
-    count = 0;
-    renderCounterApp();
-};
-
-const renderCounterApp = () => {
-    const template2 = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>reset</button>
-        </div>
-    );
-
-    ReactDOM.render(template2, appRoot);    
-};
-
-renderCounterApp();
-
+renderApp();
 
